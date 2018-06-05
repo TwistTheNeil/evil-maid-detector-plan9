3a24cff3b3c13c94a9fd3d5ef3ef0c5a55cacfd7
Neil Castelino <neil.fl93@gmail.com>
2018-06-05 13:57:11 -0400

    some changes

--- acc.c
+++ acc.c
@@ -9,8 +9,10 @@
 #define whoami 0x0D
 #define plcfg 0x11
 #define pldebounce 0x12
+#define plstatus 0x10
 #define deviceid 0x1A
 #define XYZ_DATA_CFG 0x0E
+#define F_SETUP 0x09
 
 int
 acc_bind(int *data, int *ctl) {
@@ -40,6 +42,8 @@ acc_subaddress_off(int ctl) {
 int
 acc_subaddress_on(int ctl) {
 	char subaddr_1[13] = "subaddress 1\0";
+
+// TODO use i2c_write instead
 	if(pwrite(ctl, subaddr_1, strlen(subaddr_1), 0) != strlen(subaddr_1)) {
 		print("Failed to turn on subaddressing\n");
 		return -1;
@@ -73,10 +77,28 @@ acc_read_register(int fd_data, int fd_ctl, uchar addr, uchar *buf) {
 	int ret;
 
 	acc_subaddress_on(fd_ctl);
+	sleep(3);
 	ret = pread(fd_data, buf, 1, addr);
 	acc_subaddress_off(fd_ctl);
+	sleep(3);
 
 	if(ret != 1) {
+		return -1;
+	}
+	return 0;
+}
+
+int
+acc_read_6_register(int fd_data, int fd_ctl, uchar addr, uchar *buf) {
+	int ret;
+
+	acc_subaddress_on(fd_ctl);
+	sleep(3);
+	ret = pread(fd_data, buf, 6, addr);
+	acc_subaddress_off(fd_ctl);
+	sleep(3);
+
+	if(ret != 6) {
 		return -1;
 	}
 	return 0;
@@ -88,41 +110,34 @@ acc_activate(int acc_data, int acc_ctl) {
 	uchar temp;
 	uchar *buf = calloc(1, sizeof(uchar));
 
-	print("Turning on accelerometer\n");
-	err = acc_read_register(acc_data, acc_ctl, ctrl_reg1, buf);
-	if(err < 0) {
-		print("Failed to read ctrl_reg1\n");
+	// Disable FIFO
+	/*if(i2c_write(create_i2c_write_buffer(F_SETUP, 0x00), 2, acc_data) != 2) {
+		print("Failed to disable FIFO setup\n");
 		return -1;
-	}
-	if(((buf[0]<<7)>>7) != 0x01) {
-		if(i2c_write(create_i2c_write_buffer(ctrl_reg1, (0x01 | (1<<2))), 2, acc_data) != 2) {
-			print("Failed to turn on accelerometer\n");
-			return -1;
-		}
-		print("Accelerometer is on!\n");
-	} else {
-		print("Accelerometer is already on.\n");
-	}
-
+	}*/
 
 	print("Double checking device ID");
 	err = acc_read_register(acc_data, acc_ctl, whoami, buf);
 	if(err < 0) {
-		print("Failed reading ctrl register 1\n");
+		print("\nFailed reading ctrl register 1\n");
 		return -1;
 	}
 	if(buf[0] != deviceid) {
-		print(" - Failed to turn on accelerometer (0x00)\n");
+		print("\nFailed to turn on accelerometer (0x00)\n");
 		return -1;
 	}
 	print(" - Device ID is correct!\n");
 
 	// Reset the accelerometer
-	if(i2c_write(create_i2c_write_buffer(ctrl_reg2, 0x40), 2, acc_data) != 2) { //change 40 to 1<<6
+	if(i2c_write(create_i2c_write_buffer(ctrl_reg2, 1<<6), 2, acc_data) != 2) {
 		print("Failed to reset accelerometer\n");
 		return -1;
 	}
-	sleep(300);
+	err = acc_read_register(acc_data, acc_ctl, ctrl_reg2, &temp);
+	while( (temp & (1<<6) ) == 1) {
+		err = acc_read_register(acc_data, acc_ctl, ctrl_reg2, &temp);
+		sleep(300);
+	}
 
 	if(i2c_write(create_i2c_write_buffer(XYZ_DATA_CFG, 0x01), 2, acc_data) != 2) {
 		print("Failed to enable 4g scale range\n");
@@ -130,7 +145,7 @@ acc_activate(int acc_data, int acc_ctl) {
 	}
 
 	// Set high resolution mode on
-	if(i2c_write(create_i2c_write_buffer(ctrl_reg2, 1<<2), 2, acc_data) != 2) {
+	if(i2c_write(create_i2c_write_buffer(ctrl_reg2, 1<<1), 2, acc_data) != 2) {
 		print("Failed to set high resolution\n");
 		return -1;
 	}
@@ -151,6 +166,12 @@ acc_activate(int acc_data, int acc_ctl) {
 		return -1;
 	}
 
+	// activate with low noise mode
+	print("Turning accelerometer on\n");
+	if(i2c_write(create_i2c_write_buffer(ctrl_reg1, (0x01|(1<<2))), 2, acc_data) != 2) {
+		print("Failed to enable accelerometer with low noise mode\n");
+		return -1;
+	}
 
 	return 0;
 }
@@ -178,16 +199,36 @@ acc_initialize(int *acc_data, int *acc_ctl) {
 int
 acc_get_sample(int acc_ctl, int acc_data) {
 	uchar x_msb, x_lsb, y_msb, y_lsb, z_msb, z_lsb;
+	uchar pl_status, xyz_range;
+
+	uchar x[6];
 	int read;
 
+	x_msb = 0;
+
 	read = acc_read_register(acc_data, acc_ctl, 0x01, &x_msb);
+	if(read < 0) {
+		print("failed read\n");
+	} else {
+		print("read!: %x\n", x_msb);
+	}
 	read = acc_read_register(acc_data, acc_ctl, 0x02, &x_lsb);
 	read = acc_read_register(acc_data, acc_ctl, 0x03, &y_msb);
 	read = acc_read_register(acc_data, acc_ctl, 0x04, &y_lsb);
 	read = acc_read_register(acc_data, acc_ctl, 0x05, &z_msb);
 	read = acc_read_register(acc_data, acc_ctl, 0x06, &z_lsb);
+	read = acc_read_register(acc_data, acc_ctl, plstatus, &pl_status);
+	read = acc_read_register(acc_data, acc_ctl, XYZ_DATA_CFG, &xyz_range);
 
-	print("OMG %x %x %x %x %x %x\n", x_msb, x_lsb, y_msb, y_lsb, z_msb, z_lsb);
+	print("XYZ: %x %x %x %x %x %x, PL: %x, G: %x\n", x_msb, x_lsb, y_msb, y_lsb, z_msb, z_lsb, pl_status, xyz_range);
+	
+
+	read = acc_read_6_register(acc_data, acc_ctl, 0x01, x);
+	if (read < 0) {
+		print("read_6 failed\n");
+		return -1;
+	}
+	print("R6: %x\n", x);
 	return 0;
 }
 
