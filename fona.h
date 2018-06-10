a3c1f071c328557b75a4c140bb586cb8d1fc6a0c
Neil Castelino <neil.fl93@gmail.com>
2018-06-09 20:42:44 -0400

    refactor fona

--- fona.h
+++ fona.h
@@ -1,31 +1,30 @@
-#include<u.h>
-#include<libc.h>
+void
+init_fona(int *eia_data, int *eia_ctl) {
+	*eia_data = open("/dev/eia0", ORDWR);
+	*eia_ctl = open("/dev/eia0ctl", ORDWR);
+
+	fprint(*eia_ctl, "b115200\n");
+	fprint(*eia_ctl, "l8 pn s1\n");
+}
 
 void
-main(int argc, char **argv) {
-	char wbuf[10];
+set_text_mode(int eia_data) {
+	fprint(eia_data, "AT+CMGF=1\n");
+}
+
+void
+send_sms(int eia_data, char *number) {
+	char wbuf[25];
 	char wmsg[25];
 	char ctrlz[2];
-	int eia_data = open("/dev/eia0", ORDWR);
-	int eia_ctl = open("/dev/eia0ctl", ORDWR);
-	int eia_status = open("/dev/eia0status", ORDWR);
 
-	if(argc !=2 ) {
-		print("Please pass a cell phone number as an argument\n");
-		return;
-	}
-
-	fprint(eia_ctl, "b115200\n");
-	fprint(eia_ctl, "l8 pn s1\n");
+	/* We need to send ctrl+z when finished typing message */
 	sprint(ctrlz, "%c", 0x1A);
 	ctrlz[1] = 0;
 
-	//fprint(eia_data, "AT\r\n");
-	//read(eia_data, buf, 1);
-	//buf[1]=0;
-	//print("RETURNED: %s\n", buf);
-
-	sprint(wbuf, "AT+CMGS=\"%s\"\r\n", argv[1]);
+	/* Set text mode and send message */
+	set_text_mode(eia_data);
+	sprint(wbuf, "AT+CMGS=\"%s\"\r\n", number);
 	sprint(wmsg, "Detected motion\r\n");
 
 	fprint(eia_data, wbuf);
